e08cf379a5f7045d1f92b8c23937bc4ac844948e
Mac Champion <mac.champion@cerner.com>
2015-12-14 12:51:53 -0600

    test commit, please squash

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 git fetch origin
 git reset --hard origin/master
+chmod +x checkspace
+chmod +x hcp
+chmod +x hold
+chmod +x psme
 chmod +x runjar
 chmod +x rechup
-chmod +x hcp
-chmod +x psme
-chmod +x checkspace
